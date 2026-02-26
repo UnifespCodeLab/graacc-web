6ef93acbdfa5986122999a6ede6c939df167563d
Guilherme Samuel <guisamuel800@gmail.com>
2026-02-25 22:19:32 -0300

    refactor: parametrize confirm account request to receive string or number input

--- utils/api/register/confirmAccount.ts
+++ utils/api/register/confirmAccount.ts
@@ -1,9 +1,18 @@
-export default async function confirmAccount(id_usuario: number) {
+import isNumber from "~/utils/others/isNumber";
+
+export default async function confirmAccount(data: string | number) {
   const { $api } = useNuxtApp();
+  
+  let body: any = {};
+
+  if(isNumber(data))
+    body = { id_usuario: data };
+  else
+    body = { email: data };
 
   const response = await $api("/usuarios/confirmar", {
     method: "POST",
-    body: { id_usuario }
+    body
   });
 
   return response;
